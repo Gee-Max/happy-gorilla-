e8458a4a24b451e8377a5c328313d2b0e8055411
Gee-Max <57570007+Gee-Max@users.noreply.github.com>
2019-11-09 17:27:11 +0000

    Add files via upload
    Insult generator written in Python, for fun only

---
+++ insult generator.py
@@ -0,0 +1,260 @@
+
+# Program that generates a random insult
+
+import random
+
+adjective =('Arrogant', 'Big-headed', 'Self-centred', 'Vain', 'Boastful', 'Pompous', 'Callous', 'Cynical','Overcritical', 'Patronising',
+'Aloof', 'Impolite', 'Inconsiderate', 'Thoughtless','Confrontational', 'Defensive', 'Hostile', 'Belligerent', 'Bitchy', 'Bossy', 
+'Cruel', 'Domineering', 'Deceitful', 'Dishonest', 'Machiavellian','Sneaky', 'Untrustworthy''Dogmatic', 'Inflexible', 'Intolerant',
+'Narrow-minded','Stubborn', 'Obstinate', 'Pig-headed', 'Fussy', 'Indiscreet', 'Tactless', 'Unpredictable',
+'Vague', 'Impatient','Unreliable', 'Jealous', 'Possessive', 'Resentful', 'Secretive', 'Idle', 'Careless', 'Irresponsible',
+'Untidy', 'Cowardly', 'Foolish', 'Gullible', 'Indecisive','Weak-willed', 'Quick-tempered', 'Grumpy', 'Moody', 'Pessimistic',
+'Sullen', 'Touchy', 'Finicky', 'Silly', 'Vulgar',)
+
+
+
+
+curse_word=('anal loving', 'anus licking' ,'arse licking ' ,'asshair plucking' ,'ass-fucking' ,'tea bagging'
+ ,'beastial' ,'bellend','beastiality','bestial' ,'bitch' ,'blowjob','bollock sniffing'
+ ,'boner' ,'boob' ,'bugger', 'bum','butt','butthole','buttplug','cock','cock-sucker','cockface'
+,'cockhead','cockmunch','cockmuncher','cocksucker','cocksucking','cumshot','cunt','cuntlick','cuntlicker ','cuntlicking'
+,'cunts','dick','dickhead','dildo','dogging','doosh','ejaculating','fanny','fag','fanny'
+,'fannyflap','fannyfucking','fatassed','felching','fellating','fistfucking','flange','fucker','fuckhead','fuckwhit'
+,'fuckwit','gaylord','jack-off ','jerk-off ','jism','jizz','knob','knobend','knobhead','knobjocky'
+,'masterbating','mother fucking','nazi','pecker','penis','pigfucking','pissflaps','pissing','poop flicking','rimjawed'
+,'rimming','screwing','scrote','shitdick','shithead','shitting','skank','slut','smegma sniffing','snatch'
+,'spunk munching','tittiefucker','titwanking','twathead','twatty','twunt','wanky','whore')
+
+
+noun = ('goblin','troll','cat','dog','donkey','goat','guinea pig','Horse','Pig','Ape'
+,'Baboon','Badger','Bald eagle','Dung beatle','Bat','Bear','Beaver','Bedbug','Bee','Beatle'
+,'Bird','Bison','Blackbird','Black panther','Black widow spider','sperm whale','Boa','Boar','Bobcat'
+,'Bonobo','Buffalo','Bug','Butterfly','Buzzard','Camel','Chimpanzee','Cougar','Cow','Crab'
+,'Crocodile','Crow','Cuckoo','Damselfly','Dingo','Dinosaur','Dog','Dolphin','Donkey','Dragon'
+,'Duck','Dung beetle','Eagle','Earthworm','Earwig','Eel','Elephant','Ferret','Flea','Fly'
+,'Flyingfish','Fox','Frog','Fruit bat','Giraffe','Goat','Goose','Gopher','Gorilla','Grizzly bear'
+,'Guinea pig','Hermit crab','Hippopotamus','Humpback whale','Hyena','Komodo dragon','Lizard','Llama','Lobster','Mackerel'
+,'Magpie','Mantee','Mole','Mongoose','Monitor lizard','Monkey','Mountain goat','Mouse','Mule','Muskox'
+,'Narwhal','Newt','Nightingale','Octopus','Orangutan','Orca','Ostrich','Otter','Owl','Ox'
+,'Panda','Penguin','Pheasant','Pig','Pigeon','Pike','Prawn','Raccoon','Rat','Rhinoceros'
+,'Sheep','Shrew','Shrimp','Skunk','Sloth','Slug','Snail','Snake','Sperm whale','Spider monkey'
+,'Spoonbill','Squid','Squirrel','Starfish','Sturgeon','Tick','Thrush','Tiger','Toad'
+,'Tree frog','Trout','Tuna','Turkey','Turtle','Vampire bat','Vulture','Wallaby','Walrus'
+,'Weasel','Wombat','Woodpecker','Worm','Chicken','Camel','Lab rat','Ferret','Goldfish','Guinea pig'
+,'Guppy','Llama','Sheep','Siamese fighting fish','Yak','Water buffalo',)
+
+
+x=' ';
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+
+print (ad+x+curse+x+name)
+
+ad =(random.choice(adjective))
+curse =(random.choice(curse_word))
+name =(random.choice(noun))
+print (ad+x+curse+x+name)
+
+
+
+
+
